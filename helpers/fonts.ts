30b876ffbe42380841b6d03df46965b74e9f1d95
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2026-01-09 12:49:08 +0100

    fix: add correct theme

--- helpers/fonts.ts
+++ helpers/fonts.ts
@@ -3,59 +3,14 @@ import localFont from "next/font/local";
 const displayFont = localFont({
   src: [
     {
-      path: "../token/fonts/Fredoka-Regular.woff2",
+      path: "../token/fonts/novelpro-regular.woff",
       weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-SemiBold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-SemiBold.woff",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Medium.woff",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Light.woff",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Bold.woff2",
-      weight: "bold",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Bold.woff",
-      weight: "bold",
       style: "normal",
     },
   ],
   preload: true,
   display: "swap",
-  variable: "--ks-font-family-display",
+  variable: "--ks-brand-font-family-display",
   fallback: [
     "Baskerville",
     "'Baskerville Old Face'",
@@ -69,156 +24,34 @@ const displayFont = localFont({
 const copyFont = localFont({
   src: [
     {
-      path: "../token/fonts/Mulish-Regular.woff2",
+      path: "../token/fonts/novelsanspro-regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-Regular.woff",
-      weight: "400",
+      path: "../token/fonts/novelsanspro-bold.woff",
+      weight: "700",
       style: "normal",
     },
-    {
-      path: "../token/fonts/Mulish-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
   ],
   preload: true,
   display: "swap",
-  variable: "--ks-font-family-copy",
+  variable: "--ks-brand-font-family-copy",
   fallback: ["'Helvetica Neue'", "Helvetica", "Arial", "sans-serif"],
-  adjustFontFallback: false,
-});
-
-const interfaceFont = localFont({
-  src: [
-    {
-      path: "../token/fonts/Mulish-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
-  ],
-  preload: true,
-  display: "swap",
-  variable: "--ks-font-family-interface",
-  fallback: [
-    "system-ui",
-    "-apple-system",
-    "BlinkMacSystemFont",
-    "Avenir Next",
-    "Avenir",
-    "Segoe UI",
-    "Lucida Grande",
-    "Helvetica Neue",
-    "Helvetica",
-    "Fira Sans",
-    "Roboto",
-    "Noto",
-    "Droid Sans",
-    "Cantarell",
-    "Oxygen",
-    "Ubuntu",
-    "Franklin Gothic Medium",
-    "Century Gothic",
-    "Liberation Sans",
-    "sans-serif",
-  ],
   adjustFontFallback: false,
 });
 
 const displayFontPreview = localFont({
   src: [
     {
-      path: "../token/fonts/Fredoka-Regular.woff2",
+      path: "../token/fonts/novelpro-regular.woff",
       weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-SemiBold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-SemiBold.woff",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Medium.woff",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Light.woff",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Bold.woff2",
-      weight: "bold",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Bold.woff",
-      weight: "bold",
       style: "normal",
     },
   ],
   preload: false,
   display: "auto",
-  variable: "--ks-font-family-display",
+  variable: "--ks-brand-font-family-display",
   fallback: [
     "Baskerville",
     "'Baskerville Old Face'",
@@ -232,99 +65,22 @@ const displayFontPreview = localFont({
 const copyFontPreview = localFont({
   src: [
     {
-      path: "../token/fonts/Mulish-Regular.woff2",
+      path: "../token/fonts/novelsanspro-regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-Regular.woff",
-      weight: "400",
+      path: "../token/fonts/novelsanspro-bold.woff",
+      weight: "700",
       style: "normal",
     },
-    {
-      path: "../token/fonts/Mulish-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
   ],
   preload: false,
   display: "auto",
-  variable: "--ks-font-family-copy",
+  variable: "--ks-brand-font-family-copy",
   fallback: ["'Helvetica Neue'", "Helvetica", "Arial", "sans-serif"],
   adjustFontFallback: false,
 });
 
-const interfaceFontPreview = localFont({
-  src: [
-    {
-      path: "../token/fonts/Mulish-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
-  ],
-  preload: false,
-  display: "auto",
-  variable: "--ks-font-family-interface",
-  fallback: [
-    "system-ui",
-    "-apple-system",
-    "BlinkMacSystemFont",
-    "Avenir Next",
-    "Avenir",
-    "Segoe UI",
-    "Lucida Grande",
-    "Helvetica Neue",
-    "Helvetica",
-    "Fira Sans",
-    "Roboto",
-    "Noto",
-    "Droid Sans",
-    "Cantarell",
-    "Oxygen",
-    "Ubuntu",
-    "Franklin Gothic Medium",
-    "Century Gothic",
-    "Liberation Sans",
-    "sans-serif",
-  ],
-  adjustFontFallback: false,
-});
-
-export const fontClassNames = `${displayFont.variable} ${copyFont.variable} ${interfaceFont.variable}`;
-export const fontClassNamesPreview = `${displayFontPreview.variable} ${copyFontPreview.variable} ${interfaceFontPreview.variable}`;
+export const fontClassNames = `${displayFont.variable} ${copyFont.variable}`;
+export const fontClassNamesPreview = `${displayFontPreview.variable} ${copyFontPreview.variable}`;
