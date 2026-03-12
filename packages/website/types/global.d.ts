7654bef1b9128cd044c8cec70d5f8c5a324267e8
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2026-03-12 18:35:40 +0100

    fix: type definitions for sync script

--- packages/website/types/global.d.ts
+++ packages/website/types/global.d.ts
@@ -12,3 +12,7 @@ export declare global {
     _ks: _ks;
   }
 }
+
+declare module "@kickstartds/design-system/tokens/tokensToCss.mjs" {
+  export const tokensToCss: (tokens: Record<string, unknown>) => string;
+}
